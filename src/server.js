e3923c53045a1e7e6f8dd5d7a8c3856014734842
Patrick Skiba <skibap@my.easternct.edu>
2018-02-11 08:18:41 -0800

    Broke out in to two seperate files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const https = require('https')
-const privateKey  = fs.readFileSync('./ssl/apache-selfsigned.key', 'utf8')
+const privateKey = fs.readFileSync('./ssl/apache-selfsigned.key', 'utf8')
 const certificate = fs.readFileSync('./ssl/apache-selfsigned.crt', 'utf8')
 
 const credentials = {key: privateKey, cert: certificate}
@@ -15,8 +15,8 @@ app.use(express.static(path.join(__dirname, '../dist')))
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials, app)
 
-let multer = require('multer');
-let upload = multer();
+let multer = require('multer')
+let upload = multer()
 
 app.post('/video', upload.single('video'), (req, res, next) => {
   let video = req.file
@@ -26,4 +26,3 @@ app.post('/video', upload.single('video'), (req, res, next) => {
 
 httpServer.listen(3000, () => console.log('HTTP listening on local port 3000'))
 httpsServer.listen(3001, () => console.log('HTTPS listening on local port 3001'))
-
