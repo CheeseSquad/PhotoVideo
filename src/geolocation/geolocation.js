0d43f173e685bf9d45619cea3cbd893784200556
Patrick Skiba <superninja234@gmail.com>
2018-02-12 21:15:51 -0500

    Little map!

--- src/geolocation/geolocation.js
+++ src/geolocation/geolocation.js
@@ -1,3 +1,5 @@
+var osm = require('osm');
+
 const supports_geolocation = () => {
     return !!navigator.geolocation;
 }
@@ -12,31 +14,16 @@ const get_location = () => {
 }
 
 const show_map = (position) => {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
+    let latitude = position.coords.latitude
+    let longitude = position.coords.longitude
     // let's show a map or do something interesting!
 
-    document.getElementById('geo-wrapper').css = ({ 'width': '640px', 'height': '480px' });
-
-    var latlng = new google.maps.LatLng(latitude, longitude);
-    var myOptions = {
-        zoom: 10,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById('geo-wrapper'), myOptions);
-
-    var marker = new google.maps.Marker({
-        position: latlng,
-        title: "You are here (more or less)!"
-    });
-
-    // To add the marker to the map, call setMap();
-    marker.setMap(map);
+    let map = osm().position(latitude, longitude)
 
     console.log('Lat', latitude, 'Long', longitude)
 
-    document.getElementById("msg").innerHTML = 'Your browser thinks you are here:'
+    document.getElementById('geo-wrapper').appendChild(map.show())
+    document.getElementById('msg').innerHTML = 'Your browser thinks you are here'
     document.getElementById('lat').innerHTML = 'Latitude: ' + latitude
     document.getElementById('long').innerHTML = 'Longitude: ' + longitude
 }
